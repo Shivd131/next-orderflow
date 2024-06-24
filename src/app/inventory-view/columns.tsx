574b6e429c2c1eebe7f725ad1bdb0f1cfc0bfdb0
Shivd131 <shivdeshpande131@gmail.com>
2024-06-24 23:51:50 +0530

    data table: pagination

--- src/app/inventory-view/columns.tsx
+++ src/app/inventory-view/columns.tsx
@@ -1,6 +1,16 @@
 //referred and built from https://ui.shadcn.com/docs/components/data-table
 "use client"
 import { ColumnDef } from "@tanstack/react-table"
+import { MoreHorizontal } from "lucide-react"
+import { Button } from "@/components/ui/button"
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuLabel,
+    DropdownMenuSeparator,
+    DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
 
 export type Product = {
     id: number
@@ -20,5 +30,34 @@ export const columns: ColumnDef<Product>[] = [
     {
         accessorKey: "stock",
         header: "Stock",
+    },
+    {
+        id: "actions",
+        header: "Actions",
+        cell: ({ row }) => {
+            const order = row.original
+
+            return (
+                <DropdownMenu>
+                    <DropdownMenuTrigger asChild>
+                        <Button variant="ghost" className="h-8 w-8 p-0">
+                            <span className="sr-only">Open menu</span>
+                            <MoreHorizontal className="h-4 w-4" />
+                        </Button>
+                    </DropdownMenuTrigger>
+                    <DropdownMenuContent align="end">
+                        <DropdownMenuLabel>Actions</DropdownMenuLabel>
+                        <DropdownMenuItem
+                            onClick={() => navigator.clipboard.writeText(order.id.toString())}
+                        >
+                            Copy Product ID
+                        </DropdownMenuItem>
+                        <DropdownMenuSeparator />
+                        <DropdownMenuItem>View Item</DropdownMenuItem>
+                        {/* <DropdownMenuItem>View payment details</DropdownMenuItem> */}
+                    </DropdownMenuContent>
+                </DropdownMenu>
+            )
+        },
     },
 ] 
